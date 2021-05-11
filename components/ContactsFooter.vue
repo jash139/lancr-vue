e47830c18b8a1635641e51226641ab1706dd35bc
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-12 04:08:57 +0530

    Add content to ContactsFooter

--- components/ContactsFooter.vue
+++ components/ContactsFooter.vue
@@ -1,12 +1,36 @@
 <template>
-  <div class="contacts-footer">footer</div>
+  <div class="contacts-footer">
+    <div class="stroke" />
+    <v-btn icon large>
+      <LinkedinIcon />
+    </v-btn>
+    <v-btn icon large>
+      <GithubIcon />
+    </v-btn>
+    <v-btn icon large>
+      <EmailIcon />
+    </v-btn>
+  </div>
 </template>
 
 <script>
+import LinkedinIcon from "./LinkedinIcon";
+import GithubIcon from "./GithubIcon";
+import EmailIcon from "./EmailIcon";
+
 export default {
   name: "ContactsFooter",
+  components: {
+    LinkedinIcon,
+    GithubIcon,
+    EmailIcon,
+  },
 };
 </script>
 
-<style>
+<style scoped>
+.contacts-footer {
+}
+.stroke {
+}
 </style>
