4bf161be3e5320c2368689a66a8ef9d80239a6b8
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-12 03:54:22 +0530

    Add home content and svg file

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,27 @@
 <template>
-  <h1>Welcome to Errand v2</h1>
+  <div class="hero-section">
+    <div class="cta-section">
+      <h1 class="main-heading">DESCRIBE YOUR APP IN BOLD LETTERS</h1>
+      <h2 class="sub-heading">
+        Browse projects or hire a freelancer according to your location and
+        skills.
+      </h2>
+    </div>
+    <div class="work-svg">
+      <WorkSvg />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import WorkSvg from "../components/WorkSvg";
+
+export default {
+  components: {
+    WorkSvg,
+  },
+};
 </script>
 
+<style scoped>
+</style>
