6030a6448ed0ba4aa09aba68a14ec77920160b49
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-12 03:56:48 +0530

    Add light dots svg

---
+++ components/LightDotsSvg.vue
@@ -0,0 +1,264 @@
+<template>
+  <svg
+    width="62"
+    height="60"
+    viewBox="0 0 62 60"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect
+      opacity="0.1"
+      x="4.24265"
+      width="4"
+      height="6"
+      rx="2"
+      transform="rotate(45 4.24265 0)"
+      fill="#C21E39"
+    />
+    <rect
+      opacity="0.1"
+      x="4.24265"
+      y="13.2175"
+      width="4"
+      height="6"
+      rx="2"
+      transform="rotate(45 4.24265 13.2175)"
+      fill="#C21E39"
+    />
+    <rect
+      opacity="0.1"
+      x="4.24265"
+      y="26.4351"
+      width="4"
+      height="6"
+      rx="2"
+      transform="rotate(45 4.24265 26.4351)"
+      fill="#C21E39"
+    />
+    <rect
+      opacity="0.1"
+      x="4.24265"
+      y="39.6526"
+      width="4"
+      height="6"
+      rx="2"
+      transform="rotate(45 4.24265 39.6526)"
+      fill="#C21E39"
+    />
+    <rect
+      opacity="0.1"
+      x="4.24265"
+      y="52.8701"
+      width="4"
+      height="6"
+      rx="2"
+      transform="rotate(45 4.24265 52.8701)"
+      fill="#C21E39"
+    />
+    <rect
+      opacity="0.1"
+      x="17.9602"
+      width="4"
+      height="6"
+      rx="2"
+      transform="rotate(45 17.9602 0)"
+      fill="#C21E39"
+    />
+    <rect
+      opacity="0.1"
+      x="17.9602"
+      y="13.2175"
+      width="4"
+      height="6"
+      rx="2"
+      transform="rotate(45 17.9602 13.2175)"
+      fill="#C21E39"
+    />
+    <rect
+      opacity="0.1"
+      x="17.9602"
+      y="26.4351"
+      width="4"
+      height="6"
+      rx="2"
+      transform="rotate(45 17.9602 26.4351)"
+      fill="#C21E39"
+    />
+    <rect
+      opacity="0.1"
+      x="17.9602"
+      y="39.6526"
+      width="4"
+      height="6"
+      rx="2"
+      transform="rotate(45 17.9602 39.6526)"
+      fill="#C21E39"
+    />
+    <rect
+      opacity="0.1"
+      x="17.9602"
+      y="52.8701"
+      width="4"
+      height="6"
+      rx="2"
+      transform="rotate(45 17.9602 52.8701)"
+      fill="#C21E39"
+    />
+    <rect
+      opacity="0.1"
+      x="31.6777"
+      width="4"
+      height="6"
+      rx="2"
+      transform="rotate(45 31.6777 0)"
+      fill="#C21E39"
+    />
+    <rect
+      opacity="0.1"
+      x="31.6777"
+      y="13.2175"
+      width="4"
+      height="6"
+      rx="2"
+      transform="rotate(45 31.6777 13.2175)"
+      fill="#C21E39"
+    />
+    <rect
+      opacity="0.1"
+      x="31.6777"
+      y="26.4351"
+      width="4"
+      height="6"
+      rx="2"
+      transform="rotate(45 31.6777 26.4351)"
+      fill="#C21E39"
+    />
+    <rect
+      opacity="0.1"
+      x="31.6777"
+      y="39.6526"
+      width="4"
+      height="6"
+      rx="2"
+      transform="rotate(45 31.6777 39.6526)"
+      fill="#C21E39"
+    />
+    <rect
+      opacity="0.1"
+      x="31.6777"
+      y="52.8701"
+      width="4"
+      height="6"
+      rx="2"
+      transform="rotate(45 31.6777 52.8701)"
+      fill="#C21E39"
+    />
+    <rect
+      opacity="0.1"
+      x="45.3952"
+      width="4"
+      height="6"
+      rx="2"
+      transform="rotate(45 45.3952 0)"
+      fill="#C21E39"
+    />
+    <rect
+      opacity="0.1"
+      x="45.3952"
+      y="13.2175"
+      width="4"
+      height="6"
+      rx="2"
+      transform="rotate(45 45.3952 13.2175)"
+      fill="#C21E39"
+    />
+    <rect
+      opacity="0.1"
+      x="45.3952"
+      y="26.4351"
+      width="4"
+      height="6"
+      rx="2"
+      transform="rotate(45 45.3952 26.4351)"
+      fill="#C21E39"
+    />
+    <rect
+      opacity="0.1"
+      x="45.3952"
+      y="39.6526"
+      width="4"
+      height="6"
+      rx="2"
+      transform="rotate(45 45.3952 39.6526)"
+      fill="#C21E39"
+    />
+    <rect
+      opacity="0.1"
+      x="45.3952"
+      y="52.8701"
+      width="4"
+      height="6"
+      rx="2"
+      transform="rotate(45 45.3952 52.8701)"
+      fill="#C21E39"
+    />
+    <rect
+      opacity="0.1"
+      x="59.1128"
+      width="4"
+      height="6"
+      rx="2"
+      transform="rotate(45 59.1128 0)"
+      fill="#C21E39"
+    />
+    <rect
+      opacity="0.1"
+      x="59.1128"
+      y="13.2175"
+      width="4"
+      height="6"
+      rx="2"
+      transform="rotate(45 59.1128 13.2175)"
+      fill="#C21E39"
+    />
+    <rect
+      opacity="0.1"
+      x="59.1128"
+      y="26.4351"
+      width="4"
+      height="6"
+      rx="2"
+      transform="rotate(45 59.1128 26.4351)"
+      fill="#C21E39"
+    />
+    <rect
+      opacity="0.1"
+      x="59.1128"
+      y="39.6526"
+      width="4"
+      height="6"
+      rx="2"
+      transform="rotate(45 59.1128 39.6526)"
+      fill="#C21E39"
+    />
+    <rect
+      opacity="0.1"
+      x="59.1128"
+      y="52.8701"
+      width="4"
+      height="6"
+      rx="2"
+      transform="rotate(45 59.1128 52.8701)"
+      fill="#C21E39"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "LightDotsSvg",
+};
+</script>
+
+<style>
+</style>
