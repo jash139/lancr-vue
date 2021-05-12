af867e8dfd199a108b76a6c3c829e98dfbf6c3a7
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-12 21:50:30 +0530

    Add style and content to home

--- components/LightDotsSvg.vue
+++ components/LightDotsSvg.vue
@@ -1,7 +1,7 @@
 <template>
   <svg
-    width="62"
-    height="60"
+    width="55"
+    height="55"
     viewBox="0 0 62 60"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,24 @@
     <HomeNavBar />
     <div class="hero-section">
       <div class="cta-section">
-        <h1 class="main-heading">DESCRIBE YOUR APP IN BOLD LETTERS</h1>
-        <h2 class="sub-heading">
-          Browse projects or hire a freelancer according to your location and
-          skills.
-        </h2>
-        <v-btn medium class="projects-btn">Projects</v-btn>
-        <v-btn medium class="freelancers-btn">Freelancers</v-btn>
+        <h1 class="main-heading">DESCRIBE YOUR APP <br />IN BOLD LETTERS</h1>
+        <div class="shift-right">
+          <div class="white-stroke" />
+          <div>
+            <p class="sub-heading">
+              Browse projects or hire a freelancer<br />
+              according to your location and<br />
+              skills.
+            </p>
+            <div class="cta-btns">
+              <div class="dots">
+                <LightDotsSvg />
+              </div>
+              <v-btn medium class="projects-btn">Projects</v-btn>
+              <v-btn medium class="freelancers-btn">Freelancers</v-btn>
+            </div>
+          </div>
+        </div>
       </div>
       <div class="work-svg">
         <WorkSvg />
@@ -23,12 +34,14 @@
 import WorkSvg from "../components/WorkSvg";
 import ContactsFooter from "../components/ContactsFooter";
 import HomeNavBar from "../components/HomeNavBar";
+import LightDotsSvg from "../components/LightDotsSvg";
 
 export default {
   components: {
     WorkSvg,
     ContactsFooter,
     HomeNavBar,
+    LightDotsSvg,
   },
 };
 </script>
@@ -40,9 +53,37 @@ export default {
 .hero-section {
   display: flex;
   align-items: center;
-  justify-content: space-evenly;
+  justify-content: space-around;
   margin: auto;
   max-width: 95%;
+}
+.main-heading {
+  color: #050303;
+  margin: 1.5rem 0;
+  text-transform: uppercase;
+}
+.shift-right {
+  display: flex;
+}
+.white-stroke {
+  background-color: #c21e39;
+  border-radius: 5rem;
+  height: 0.2rem;
+  margin: 0.5rem 1rem 0 0;
+  width: 2rem;
+}
+.sub-heading {
+  color: #5d5755;
+  font-size: 1rem;
+  font-weight: 700;
+}
+.cta-btns {
+  position: relative;
+}
+.dots {
+  bottom: -35px;
+  left: -35px;
+  position: absolute;
 }
 @media only screen and (max-width: 960px) {
   .hero-section {
@@ -53,7 +94,7 @@ export default {
 </style>
 
 <style>
-#app {
+body {
   font-family: "Raleway", sans-serif;
 }
 </style>
