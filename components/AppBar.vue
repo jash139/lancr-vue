0b862b17a811c222ce37cc9df24cb83c9ae17787
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-13 19:23:17 +0530

    Add content and styling to AppBar

--- components/AppBar.vue
+++ components/AppBar.vue
@@ -1,12 +1,50 @@
 <template>
-  <h1>appbar</h1>
+  <header class="header">
+    <nav class="nav">
+      <Logo />
+      <div class="right-section">
+        <button elevation="0" class="btn outlined-btn sign-btn">Signin</button>
+        <button elevation="0" class="btn secondary-btn sign-btn">Signup</button>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script>
+import Logo from "./Logo";
+
 export default {
-  name: "Appbar",
+  name: "AppBar",
+  components: {
+    Logo,
+  },
 };
 </script>
 
-<style>
+<style scoped>
+.header {
+  background: transparent;
+  border-bottom: 2px solid #ffffff;
+  backdrop-filter: blur(0.5rem);
+  position: sticky;
+  top: 0;
+  z-index: 5;
+}
+.sign-btn {
+  margin-left: 1.5rem;
+}
+.nav {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: auto;
+  min-height: 10vh;
+  width: 95%;
+  position: relative;
+}
+.right-section {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
 </style>
