fc120f31366f30a9ed860109afa97eb6e62a899f
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-15 02:41:56 +0530

    Add app bar and content style

--- pages/freelancers/index.vue
+++ pages/freelancers/index.vue
@@ -1,16 +1,31 @@
 <template>
-  <FreelancerCard />
+  <div class="root">
+    <AppBar />
+    <div class="content">
+      <FreelancerCard />
+    </div>
+  </div>
 </template>
 
 <script>
+import AppBar from "../../components/AppBar";
 import FreelancerCard from "../../components/FreelancerCard";
 
 export default {
   components: {
+    AppBar,
     FreelancerCard,
   },
 };
 </script>
 
-<style>
+<style scoped>
+.root {
+  min-height: 100vh;
+  padding-bottom: 1.5rem;
+}
+.content {
+  max-width: 95%;
+  margin: auto;
+}
 </style>
