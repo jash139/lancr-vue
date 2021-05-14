dfd04c990eecb0fadfd3dbbbab36117f4af2ae76
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-14 16:18:18 +0530

    Add styling to signin card

--- pages/signin/index.vue
+++ pages/signin/index.vue
@@ -4,7 +4,9 @@
       <h2 class="heading">Sign in</h2>
       <p class="message">
         Welcome back!<br />Did you
-        <NuxtLink to="/forget-password"> forget your password? </NuxtLink>
+        <NuxtLink to="/forget-password" class="link">
+          forget your password?
+        </NuxtLink>
       </p>
       <!-- <p class="error">{{ error }}</p> -->
 
@@ -19,12 +21,15 @@
         @click:append="show = !show"
       />
 
-      <button class="btn primary-btn btn-shadow" @click="handleSubmit">
+      <button
+        class="btn primary-btn btn-shadow login-btn"
+        @click="handleSubmit"
+      >
         Login
       </button>
       <p class="message">
         Don't have an account?
-        <NuxtLink to="/signup">Create a new account</NuxtLink>
+        <NuxtLink to="/signup" class="link">Create a new account</NuxtLink>
       </p>
     </div>
   </div>
@@ -46,12 +51,32 @@ export default {
 </script>
 
 <style>
-/* .card {
-  min-height: 600px;
-  max-width: 400px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-} */
+.card {
+  background-color: #ffffff;
+  box-shadow: 0 10px 6px rgba(0, 0, 0, 0.05);
+  border-radius: 3px;
+  max-width: 450px;
+  margin: 0 1rem;
+  padding: 2rem;
+  text-align: center;
+}
+.heading {
+  color: #c21e39;
+  font-size: 1.6rem;
+  margin: 1rem 0;
+}
+.message {
+  color: #5d5755;
+  font-size: 0.9rem;
+  font-weight: 700;
+  line-height: 1.5;
+}
+.login-btn {
+  margin: 1rem 0;
+  width: 100%;
+}
+.link {
+  color: #c21e39;
+  text-decoration: none;
+}
 </style>
