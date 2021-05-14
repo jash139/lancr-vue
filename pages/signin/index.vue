dd51d5c8fc75f80f461aa74f8a4fef4531ce7883
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-14 16:22:03 +0530

    Add AppBar to signin page

--- pages/signin/index.vue
+++ pages/signin/index.vue
@@ -1,41 +1,46 @@
 <template>
   <div class="root">
-    <div class="card">
-      <h2 class="heading">Sign in</h2>
-      <p class="message">
-        Welcome back!<br />Did you
-        <NuxtLink to="/forget-password" class="link">
-          forget your password?
-        </NuxtLink>
-      </p>
-      <!-- <p class="error">{{ error }}</p> -->
+    <AppBar />
+    <div class="content">
+      <div class="card">
+        <h2 class="heading">Sign in</h2>
+        <p class="message">
+          Welcome back!<br />Did you
+          <NuxtLink to="/forget-password" class="link">
+            forget your password?
+          </NuxtLink>
+        </p>
+        <!-- <p class="error">{{ error }}</p> -->
 
-      <v-text-field v-model="email" label="Email" />
-      <v-text-field
-        v-model="password"
-        :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-        :type="show ? 'text' : 'password'"
-        name="input-10-1"
-        label="Password"
-        counter
-        @click:append="show = !show"
-      />
+        <v-text-field v-model="email" label="Email" />
+        <v-text-field
+          v-model="password"
+          :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+          :type="show ? 'text' : 'password'"
+          name="input-10-1"
+          label="Password"
+          counter
+          @click:append="show = !show"
+        />
 
-      <button
-        class="btn primary-btn btn-shadow login-btn"
-        @click="handleSubmit"
-      >
-        Login
-      </button>
-      <p class="message">
-        Don't have an account?
-        <NuxtLink to="/signup" class="link">Create a new account</NuxtLink>
-      </p>
+        <button
+          class="btn primary-btn btn-shadow login-btn"
+          @click="handleSubmit"
+        >
+          Login
+        </button>
+        <p class="message">
+          Don't have an account?
+          <NuxtLink to="/signup" class="link">Create a new account</NuxtLink>
+        </p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import AppBar from "../../components/AppBar";
+
 export default {
   data() {
     return {
@@ -44,6 +49,9 @@ export default {
       password: "",
     };
   },
+  components: {
+    AppBar,
+  },
   methods: {
     handleSubmit: () => console.log("submit"),
   },
@@ -51,12 +59,17 @@ export default {
 </script>
 
 <style>
+.root {
+}
+.content {
+  max-width: 95%;
+  margin: auto;
+}
 .card {
   background-color: #ffffff;
   box-shadow: 0 10px 6px rgba(0, 0, 0, 0.05);
   border-radius: 3px;
   max-width: 450px;
-  margin: 0 1rem;
   padding: 2rem;
   text-align: center;
 }
