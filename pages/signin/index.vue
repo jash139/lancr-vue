29d0d2d15079e75bc0e9a0336eca58c70ef38779
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-14 16:10:09 +0530

    Add content to signin page

--- pages/signin/index.vue
+++ pages/signin/index.vue
@@ -1,10 +1,57 @@
 <template>
-  <h1>signin page</h1>
+  <div class="root">
+    <div class="card">
+      <h2 class="heading">Sign in</h2>
+      <p class="message">
+        Welcome back!<br />Did you
+        <NuxtLink to="/forget-password"> forget your password? </NuxtLink>
+      </p>
+      <!-- <p class="error">{{ error }}</p> -->
+
+      <v-text-field v-model="email" label="Email" />
+      <v-text-field
+        v-model="password"
+        :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+        :type="show ? 'text' : 'password'"
+        name="input-10-1"
+        label="Password"
+        counter
+        @click:append="show = !show"
+      />
+
+      <button class="btn primary-btn btn-shadow" @click="handleSubmit">
+        Login
+      </button>
+      <p class="message">
+        Don't have an account?
+        <NuxtLink to="/signup">Create a new account</NuxtLink>
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      show: true,
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    handleSubmit: () => console.log("submit"),
+  },
+};
 </script>
 
 <style>
+/* .card {
+  min-height: 600px;
+  max-width: 400px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+} */
 </style>
