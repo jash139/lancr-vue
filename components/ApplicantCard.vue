c758ada67081f8a756d61e42cc17e673ed16d551
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-15 23:52:05 +0530

    Add connect button and styles

--- components/ApplicantCard.vue
+++ components/ApplicantCard.vue
@@ -12,6 +12,31 @@
           <div class="role">Underworld don</div>
         </div>
       </div>
+      <v-btn icon class="connect-btn">
+        <svg
+          width="20"
+          height="20"
+          viewBox="0 0 88 73"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M0 68.8333C0 57.3274 9.3274 48 20.8333 48H56.1667C67.6726 48 77 57.3274 77 68.8333V68.8333C77 71.1345 75.1345 73 72.8333 73H4.16666C1.86548 73 0 71.1345 0 68.8333V68.8333Z"
+            fill="#5D5755"
+          />
+          <circle cx="39" cy="22" r="22" fill="#5D5755" />
+          <rect x="78" y="22" width="3" height="18" rx="1.5" fill="#5D5755" />
+          <rect
+            x="70"
+            y="32"
+            width="3"
+            height="18"
+            rx="1.5"
+            transform="rotate(-90 70 32)"
+            fill="#5D5755"
+          />
+        </svg>
+      </v-btn>
     </div>
     <div class="divider" />
   </div>
@@ -24,6 +49,12 @@ export default {
 </script>
 
 <style scoped>
+.applicant-card {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 1.5rem 0;
+}
 .body {
   display: flex;
 }
@@ -51,6 +82,11 @@ export default {
   font-size: 0.7rem;
   font-weight: 600;
   padding: 0.2rem 1rem;
+}
+.connect-btn {
+  border: 1px solid #cdcdcd;
+  height: 35px;
+  width: 35px;
 }
 .divider {
   background-color: #f2e9e6;
