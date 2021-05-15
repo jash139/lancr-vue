df2726ab498eeceada80929c0c622684c4ad4bee
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-15 16:09:13 +0530

    Add projects page layout

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,10 +1,59 @@
 <template>
-  <h1>Projects</h1>
+  <div class="root">
+    <AppBar />
+    <div class="content">
+      <div class="projects">
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import AppBar from "../../components/AppBar";
+import ProjectCard from "../../components/ProjectCard";
+
+export default {
+  components: {
+    AppBar,
+    ProjectCard,
+  },
+};
 </script>
 
-<style>
+<style scoped>
+.root {
+  min-height: 100vh;
+  padding-bottom: 1.5rem;
+}
+.content {
+  max-width: 95%;
+  margin: auto;
+}
+.projects {
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: auto auto auto auto;
+}
+@media only screen and (max-width: 1400px) {
+  .projects {
+    grid-template-columns: auto auto auto;
+  }
+}
+@media only screen and (max-width: 1080px) {
+  .projects {
+    grid-template-columns: auto auto;
+  }
+}
+@media only screen and (max-width: 700px) {
+  .projects {
+    grid-template-columns: auto;
+  }
+}
 </style>
