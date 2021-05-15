26fa8948247fcba1d6a34b7a06acc96f41e434a9
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-16 02:48:55 +0530

    Add dropdown icon and style

--- components/SortByDropdown.vue
+++ components/SortByDropdown.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="dropdown">
-    <button @click="toggleMenu()" class="dropdown-btn btn">Dropdown</button>
+    <button @click="toggleMenu()" class="dropdown-btn btn">
+      Dropdown
+      <svg
+        width="10"
+        height="10"
+        viewBox="0 0 25 17"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M1.05362 3.63029C0.738528 3.27624 0.564453 2.8188 0.564453 2.34484V2.34484C0.564452 0.589392 2.71518 -0.257682 3.91227 1.02629L12.5645 10.3065L21.2166 1.02629C22.4137 -0.257682 24.5645 0.589391 24.5645 2.34484V2.34484C24.5645 2.8188 24.3904 3.27624 24.0753 3.63029L13.3115 15.7251C12.9137 16.1721 12.2152 16.172 11.8174 15.7251L1.05362 3.63029Z"
+          fill="#5D5755"
+        />
+      </svg>
+    </button>
     <div id="myDropdown" class="dropdown-content card-shadow">
       <a href="#home">Home</a>
       <a href="#about">About</a>
@@ -38,12 +52,16 @@ export default {
 
 <style scoped>
 .dropdown-btn {
-  background-color: #3498db;
-  color: white;
-  padding: 16px;
-  font-size: 16px;
-  border: none;
-  cursor: pointer;
+  border: 1px solid #5d5755;
+  border-radius: 5rem;
+  color: #5d5755;
+  display: flex;
+  align-items: center;
+  font-size: 0.8rem;
+  font-weight: 700;
+  outline: none;
+  padding: 0.4rem 1rem;
+  text-transform: none;
 }
 .dropdown-btn:focus {
   background-color: #2980b9;
@@ -55,12 +73,12 @@ export default {
 }
 
 .dropdown-content {
+  background-color: #ffffff;
+  border-radius: 3px;
   display: none;
-  position: absolute;
-  background-color: #f1f1f1;
   min-width: 160px;
   overflow: auto;
-  /* box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2); */
+  position: absolute;
   z-index: 1;
 }
 
