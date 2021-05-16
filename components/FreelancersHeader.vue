7c8d9a3cb44799c69142c655e07010b3c7939137
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-16 15:14:03 +0530

    Add content to freelancers header

--- components/FreelancersHeader.vue
+++ components/FreelancersHeader.vue
@@ -1,10 +1,20 @@
 <template>
-  <h2>Freelancers Header</h2>
+  <div class="freelancers-header">
+    <h2 class="heading">Freelancers</h2>
+    <div class="actions">
+      <SortByDropdown />
+    </div>
+  </div>
 </template>
 
 <script>
+import SortByDropdown from "./SortByDropdown";
+
 export default {
   name: "FreelancersHeader",
+  components: {
+    SortByDropdown,
+  },
 };
 </script>
 
