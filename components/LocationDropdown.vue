1fc24c54975da9c9c732d4106a9eceeb8189cb0d
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-17 03:17:57 +0530

    Refactor code to use Buefy dropdown

--- components/LocationDropdown.vue
+++ components/LocationDropdown.vue
@@ -1,29 +1,30 @@
 <template>
-  <div class="dropdown">
-    <button class="location-dropdown-btn btn">
-      Location
-      <div class="drop-icon">
-        <svg
-          width="10"
-          height="10"
-          viewBox="0 0 25 17"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M1.05362 3.63029C0.738528 3.27624 0.564453 2.8188 0.564453 2.34484V2.34484C0.564452 0.589392 2.71518 -0.257682 3.91227 1.02629L12.5645 10.3065L21.2166 1.02629C22.4137 -0.257682 24.5645 0.589391 24.5645 2.34484V2.34484C24.5645 2.8188 24.3904 3.27624 24.0753 3.63029L13.3115 15.7251C12.9137 16.1721 12.2152 16.172 11.8174 15.7251L1.05362 3.63029Z"
-            fill="#cdcdcd"
-          />
-        </svg>
-      </div>
-    </button>
-    <div class="dropdown-content card-shadow">
-      <p class="dropdown-item" @click="setSelectedOption('None')">None</p>
-      <p class="dropdown-item" @click="setSelectedOption('Active')">Active</p>
-      <p class="dropdown-item" @click="setSelectedOption('Closed')">Closed</p>
-      <p class="dropdown-item" @click="setSelectedOption('Recent')">Recent</p>
-    </div>
-  </div>
+  <b-dropdown
+    :scrollable="isScrollable"
+    :max-height="maxHeight"
+    v-model="selectedOption"
+  >
+    <template #trigger>
+      <b-button
+        class="location-dropdown-btn btn"
+        :label="selectedOption"
+        icon-right="menu-down"
+      />
+    </template>
+
+    <b-dropdown-item @click="setSelectedOption('None')">
+      <p class="dropdown-item">None</p>
+    </b-dropdown-item>
+    <b-dropdown-item @click="setSelectedOption('Active')">
+      <p class="dropdown-item">Active</p>
+    </b-dropdown-item>
+    <b-dropdown-item @click="setSelectedOption('Closed')">
+      <p class="dropdown-item">Closed</p>
+    </b-dropdown-item>
+    <b-dropdown-item @click="setSelectedOption('Recent')">
+      <p class="dropdown-item">Recent</p>
+    </b-dropdown-item>
+  </b-dropdown>
 </template>
 
 <script>
@@ -31,7 +32,9 @@ export default {
   name: "LocationDropdown",
   data() {
     return {
-      selectedOption: "None",
+      isScrollable: true,
+      maxHeight: 300,
+      selectedOption: "Country",
     };
   },
   methods: {
@@ -43,13 +46,6 @@ export default {
 </script>
 
 <style scoped>
-.dropdown {
-  position: relative;
-  display: inline-block;
-}
-.dropdown:hover .dropdown-content {
-  display: block;
-}
 .location-dropdown-btn {
   background-color: #ffffff;
   border: 1px solid #cdcdcd;
@@ -65,30 +61,12 @@ export default {
   padding: 0.4rem 1rem;
   text-transform: none;
 }
-.drop-icon {
-  margin-left: 0.5rem;
-}
 .location-dropdown-btn:focus {
   background-color: #fcfcfc;
 }
-.dropdown-content {
-  background-color: #ffffff;
-  border-radius: 3px;
-  display: none;
-  max-height: 300px;
-  min-width: 150px;
-  overflow: auto;
-  position: absolute;
-  z-index: 1;
-}
 .dropdown-item {
   color: #5d5755;
-  cursor: pointer;
   font-size: 0.9rem;
   font-weight: 600;
-  padding: 0.6rem 1rem;
-}
-.dropdown-item:hover {
-  background-color: #ececec;
 }
 </style>
