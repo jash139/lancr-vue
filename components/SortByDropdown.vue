bac4f1b3c3452d50e9a893aef02578353bba78f6
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-17 02:46:29 +0530

    Refactor code to use buefy dropdown

--- components/SortByDropdown.vue
+++ components/SortByDropdown.vue
@@ -1,31 +1,32 @@
 <template>
   <div class="sort-by-dropdown">
     <p class="sortby">Sort By</p>
-    <div class="dropdown">
-      <button class="sortby-dropdown-btn btn">
-        {{ selectedOption }}
-        <div class="drop-icon">
-          <svg
-            width="10"
-            height="10"
-            viewBox="0 0 25 17"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M1.05362 3.63029C0.738528 3.27624 0.564453 2.8188 0.564453 2.34484V2.34484C0.564452 0.589392 2.71518 -0.257682 3.91227 1.02629L12.5645 10.3065L21.2166 1.02629C22.4137 -0.257682 24.5645 0.589391 24.5645 2.34484V2.34484C24.5645 2.8188 24.3904 3.27624 24.0753 3.63029L13.3115 15.7251C12.9137 16.1721 12.2152 16.172 11.8174 15.7251L1.05362 3.63029Z"
-              fill="#cdcdcd"
-            />
-          </svg>
-        </div>
-      </button>
-      <div class="dropdown-content card-shadow">
-        <p class="dropdown-item" @click="setSelectedOption('None')">None</p>
-        <p class="dropdown-item" @click="setSelectedOption('Active')">Active</p>
-        <p class="dropdown-item" @click="setSelectedOption('Closed')">Closed</p>
-        <p class="dropdown-item" @click="setSelectedOption('Recent')">Recent</p>
-      </div>
-    </div>
+    <b-dropdown
+      :scrollable="isScrollable"
+      :max-height="maxHeight"
+      v-model="selectedOption"
+    >
+      <template #trigger>
+        <b-button
+          class="sortby-dropdown-btn btn"
+          :label="selectedOption"
+          icon-right="menu-down"
+        />
+      </template>
+
+      <b-dropdown-item @click="setSelectedOption('None')">
+        <p class="dropdown-item">None</p>
+      </b-dropdown-item>
+      <b-dropdown-item @click="setSelectedOption('Active')">
+        <p class="dropdown-item">Active</p>
+      </b-dropdown-item>
+      <b-dropdown-item @click="setSelectedOption('Closed')">
+        <p class="dropdown-item">Closed</p>
+      </b-dropdown-item>
+      <b-dropdown-item @click="setSelectedOption('Recent')">
+        <p class="dropdown-item">Recent</p>
+      </b-dropdown-item>
+    </b-dropdown>
   </div>
 </template>
 
@@ -34,6 +35,8 @@ export default {
   name: "SortByDropdown",
   data() {
     return {
+      isScrollable: true,
+      maxHeight: 300,
       selectedOption: "None",
     };
   },
@@ -55,13 +58,7 @@ export default {
   font-size: 0.9rem;
   font-weight: 600;
   margin-right: 0.5rem;
-}
-.dropdown {
-  position: relative;
-  display: inline-block;
-}
-.dropdown:hover .dropdown-content {
-  display: block;
+  margin-bottom: 0 !important;
 }
 .sortby-dropdown-btn {
   background-color: #ffffff;
@@ -78,29 +75,13 @@ export default {
   padding: 0.4rem 1rem;
   text-transform: none;
 }
-.drop-icon {
-  margin-left: 0.5rem;
-}
 .sortby-dropdown-btn:focus {
   background-color: #fcfcfc;
-}
-.dropdown-content {
-  background-color: #ffffff;
-  border-radius: 3px;
-  display: none;
-  min-width: 150px;
-  overflow: auto;
-  position: absolute;
-  z-index: 1;
 }
 .dropdown-item {
   color: #5d5755;
   cursor: pointer;
   font-size: 0.9rem;
-  font-weight: 600;
-  padding: 0.6rem 1rem;
-}
-.dropdown-item:hover {
-  background-color: #ececec;
+  font-weight: 500;
 }
 </style>
