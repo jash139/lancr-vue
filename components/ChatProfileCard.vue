83f77d74d54b7227f9677d7907f8cf2d10a42c5f
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-18 01:56:57 +0530

    Remove role and add hover indicator

--- components/ChatProfileCard.vue
+++ components/ChatProfileCard.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="chat-profile-card">
     <div class="body">
+      <div class="indicator" />
       <img
         src="https://images.unsplash.com/photo-1563497425252-36b755215241?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
         alt="Avatar"
         class="avatar"
       />
-      <div class="details">
-        <h2 class="name">Doggo Gang</h2>
-        <div class="role">Underworld don</div>
-      </div>
+      <h2 class="name">Doggo Gang</h2>
     </div>
     <div class="divider" />
   </div>
@@ -25,34 +23,29 @@ export default {
 .chat-profile-card {
   cursor: pointer;
 }
+.indicator {
+  background-color: transparent;
+  border-radius: 2rem;
+  max-height: 50px;
+  width: 2px;
+}
+.chat-profile-card:hover .indicator {
+  background-color: #c21e39;
+}
 .body {
   display: flex;
-  padding: 1rem 0;
-}
-.details {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: space-between;
-  margin-left: 1rem;
+  padding: 0.8rem 0;
 }
 .avatar {
   border-radius: 20rem;
+  margin-left: 0.5rem;
+  margin-right: 1rem;
   max-width: 50px;
 }
 .name {
   color: #5d5755;
   font-size: 1rem;
   font-weight: 700;
-}
-.role {
-  background-color: #c21e39;
-  border-radius: 5rem;
-  color: #ffffff;
-  display: inline-block;
-  font-size: 0.7rem;
-  font-weight: 600;
-  padding: 0.2rem 1rem;
 }
 .divider {
   background-color: #f2e9e6;
