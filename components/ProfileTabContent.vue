3b7cec11e0e1ff7fdcf8bfcb90369bd52c1ebe78
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-17 17:30:49 +0530

    Add experience and style

--- components/ProfileTabContent.vue
+++ components/ProfileTabContent.vue
@@ -5,6 +5,64 @@
         <h4 class="heading">Experience</h4>
         <div class="stroke" />
       </div>
+      <div class="exp">
+        <div class="experience">
+          <div>
+            <h3 class="org">Netflix</h3>
+            <p class="role">Full Stack Developer</p>
+          </div>
+          <p class="duration">May 2000 - Dec 2001</p>
+        </div>
+        <div class="experience">
+          <div>
+            <h3 class="org">Netflix</h3>
+            <p class="role">Full Stack Developer</p>
+          </div>
+          <p class="duration">May 2000 - Dec 2001</p>
+        </div>
+        <div class="experience">
+          <div>
+            <h3 class="org">Netflix</h3>
+            <p class="role">Full Stack Developer</p>
+          </div>
+          <p class="duration">May 2000 - Dec 2001</p>
+        </div>
+        <div class="experience">
+          <div>
+            <h3 class="org">Netflix</h3>
+            <p class="role">Full Stack Developer</p>
+          </div>
+          <p class="duration">May 2000 - Dec 2001</p>
+        </div>
+        <div class="experience">
+          <div>
+            <h3 class="org">Netflix</h3>
+            <p class="role">Full Stack Developer</p>
+          </div>
+          <p class="duration">May 2000 - Dec 2001</p>
+        </div>
+        <div class="experience">
+          <div>
+            <h3 class="org">Netflix</h3>
+            <p class="role">Full Stack Developer</p>
+          </div>
+          <p class="duration">May 2000 - Dec 2001</p>
+        </div>
+        <div class="experience">
+          <div>
+            <h3 class="org">Netflix</h3>
+            <p class="role">Full Stack Developer</p>
+          </div>
+          <p class="duration">May 2000 - Dec 2001</p>
+        </div>
+        <div class="experience">
+          <div>
+            <h3 class="org">Netflix</h3>
+            <p class="role">Full Stack Developer</p>
+          </div>
+          <p class="duration">May 2000 - Dec 2001</p>
+        </div>
+      </div>
     </div>
     <div class="card-section">
       <div class="heading-div">
@@ -65,5 +123,39 @@ export default {
   height: 0.5px;
   margin-left: 1rem;
   width: 100%;
+}
+.experience {
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  margin: 1.5rem 0;
+}
+.org {
+  color: #5d5755;
+  font-size: 1.4rem;
+}
+.role {
+  color: #5d5755;
+  font-size: 1rem;
+  font-style: italic;
+  font-weight: 500;
+}
+.duration {
+  color: #5d5755;
+  font-size: 0.9rem;
+  font-weight: 500;
+}
+.exp {
+  max-height: 350px;
+  overflow-y: auto;
+}
+.exp::-webkit-scrollbar {
+  width: 1.5rem;
+}
+.exp::-webkit-scrollbar-thumb {
+  background-color: #e5ecee;
+  border-radius: 1rem;
+  border: 9px solid transparent;
+  background-clip: content-box;
 }
 </style>
