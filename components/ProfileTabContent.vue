1b4c0c0da65fd244872d46a956cf043b60c04de2
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-17 16:51:22 +0530

    Add style and minimal content

--- components/ProfileTabContent.vue
+++ components/ProfileTabContent.vue
@@ -1,5 +1,12 @@
 <template>
-  <h2>Profile Tab Content</h2>
+  <div class="tab-card">
+    <div class="card-section">
+      <div class="heading-div">
+        <h4 class="heading">Experience</h4>
+        <div class="stroke" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,5 +15,27 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.tab-card {
+  margin: auto;
+  max-width: 900px;
+  padding: 2rem;
+}
+.heading-div {
+  display: flex;
+  align-items: center;
+  margin: 0.8rem 0;
+}
+.heading {
+  color: #5d5755;
+  font-size: 0.9rem;
+  font-weight: 700;
+}
+.stroke {
+  background-color: #f2e9e6;
+  border-radius: 2rem;
+  height: 0.5px;
+  margin-left: 1rem;
+  width: 100%;
+}
 </style>
