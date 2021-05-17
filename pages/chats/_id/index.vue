5eb3bf5bb9b7a60446950f7b8d1dc46cde66fe20
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-17 19:41:04 +0530

    Create chat pages

---
+++ pages/chats/_id/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="chats-page">
+    <AppBar />
+    <div class="content">
+      <BackButton />
+      chats{{ $route.params.id }}
+    </div>
+  </div>
+</template>
+
+<script>
+import AppBar from "../../../components/AppBar";
+import BackButton from "../../../components/BackButton";
+
+export default {
+  components: {
+    AppBar,
+    BackButton,
+  },
+};
+</script>
+
+<style scoped>
+.chats-page {
+  min-height: 100vh;
+}
+.content {
+  max-width: 95%;
+  margin: auto;
+}
+</style>
---
+++ pages/chats/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="chats-page">
+    <AppBar />
+    <div class="content">
+      <BackButton />
+      chats
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.chats-page {
+  min-height: 100vh;
+}
+.content {
+  max-width: 95%;
+  margin: auto;
+}
+</style>
