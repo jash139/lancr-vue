cf8be2b34099cc63786c89ebcc6cc4d00f954026
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-17 19:29:47 +0530

    Add content and style to connections tab

--- components/ConnectionsTabContent.vue
+++ components/ConnectionsTabContent.vue
@@ -1,5 +1,160 @@
 <template>
-  <h2>Connections tab content</h2>
+  <div class="tab-card">
+    <div class="tab-content">
+      <div>
+        <div class="connection-card">
+          <div class="body">
+            <img
+              src="https://images.unsplash.com/photo-1563497425252-36b755215241?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+              alt="Avatar"
+              class="avatar"
+            />
+            <div class="details">
+              <h2 class="name">Doggo Gang</h2>
+              <div class="role">Underworld don</div>
+            </div>
+          </div>
+          <v-btn icon class="message-btn">
+            <b-icon icon="send"> </b-icon>
+          </v-btn>
+        </div>
+        <div class="divider" />
+      </div>
+      <div>
+        <div class="connection-card">
+          <div class="body">
+            <img
+              src="https://images.unsplash.com/photo-1563497425252-36b755215241?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+              alt="Avatar"
+              class="avatar"
+            />
+            <div class="details">
+              <h2 class="name">Doggo Gang</h2>
+              <div class="role">Underworld don</div>
+            </div>
+          </div>
+          <v-btn icon class="message-btn">
+            <b-icon icon="send"> </b-icon>
+          </v-btn>
+        </div>
+        <div class="divider" />
+      </div>
+      <div>
+        <div class="connection-card">
+          <div class="body">
+            <img
+              src="https://images.unsplash.com/photo-1563497425252-36b755215241?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+              alt="Avatar"
+              class="avatar"
+            />
+            <div class="details">
+              <h2 class="name">Doggo Gang</h2>
+              <div class="role">Underworld don</div>
+            </div>
+          </div>
+          <v-btn icon class="message-btn">
+            <b-icon icon="send"> </b-icon>
+          </v-btn>
+        </div>
+        <div class="divider" />
+      </div>
+      <div>
+        <div class="connection-card">
+          <div class="body">
+            <img
+              src="https://images.unsplash.com/photo-1563497425252-36b755215241?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+              alt="Avatar"
+              class="avatar"
+            />
+            <div class="details">
+              <h2 class="name">Doggo Gang</h2>
+              <div class="role">Underworld don</div>
+            </div>
+          </div>
+          <v-btn icon class="message-btn">
+            <b-icon icon="send"> </b-icon>
+          </v-btn>
+        </div>
+        <div class="divider" />
+      </div>
+      <div>
+        <div class="connection-card">
+          <div class="body">
+            <img
+              src="https://images.unsplash.com/photo-1563497425252-36b755215241?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+              alt="Avatar"
+              class="avatar"
+            />
+            <div class="details">
+              <h2 class="name">Doggo Gang</h2>
+              <div class="role">Underworld don</div>
+            </div>
+          </div>
+          <v-btn icon class="message-btn">
+            <b-icon icon="send"> </b-icon>
+          </v-btn>
+        </div>
+        <div class="divider" />
+      </div>
+      <div>
+        <div class="connection-card">
+          <div class="body">
+            <img
+              src="https://images.unsplash.com/photo-1563497425252-36b755215241?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+              alt="Avatar"
+              class="avatar"
+            />
+            <div class="details">
+              <h2 class="name">Doggo Gang</h2>
+              <div class="role">Underworld don</div>
+            </div>
+          </div>
+          <v-btn icon class="message-btn">
+            <b-icon icon="send"> </b-icon>
+          </v-btn>
+        </div>
+        <div class="divider" />
+      </div>
+      <div>
+        <div class="connection-card">
+          <div class="body">
+            <img
+              src="https://images.unsplash.com/photo-1563497425252-36b755215241?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+              alt="Avatar"
+              class="avatar"
+            />
+            <div class="details">
+              <h2 class="name">Doggo Gang</h2>
+              <div class="role">Underworld don</div>
+            </div>
+          </div>
+          <v-btn icon class="message-btn">
+            <b-icon icon="send"> </b-icon>
+          </v-btn>
+        </div>
+        <div class="divider" />
+      </div>
+      <div>
+        <div class="connection-card">
+          <div class="body">
+            <img
+              src="https://images.unsplash.com/photo-1563497425252-36b755215241?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+              alt="Avatar"
+              class="avatar"
+            />
+            <div class="details">
+              <h2 class="name">Doggo Gang</h2>
+              <div class="role">Underworld don</div>
+            </div>
+          </div>
+          <v-btn icon class="message-btn">
+            <b-icon icon="send"> </b-icon>
+          </v-btn>
+        </div>
+        <div class="divider" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,4 +164,67 @@ export default {
 </script>
 
 <style scoped>
+.tab-card {
+  margin: auto;
+  max-width: 900px;
+  padding: 2rem;
+}
+.tab-content {
+  max-height: 500px;
+  overflow-y: auto;
+}
+.tab-content::-webkit-scrollbar {
+  width: 1.5rem;
+}
+.tab-content::-webkit-scrollbar-thumb {
+  background-color: #e5ecee;
+  border-radius: 1rem;
+  border: 9px solid transparent;
+  background-clip: content-box;
+}
+.connection-card {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 1.5rem 0;
+}
+.body {
+  display: flex;
+}
+.details {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+  margin-left: 1rem;
+}
+.avatar {
+  border-radius: 20rem;
+  max-width: 50px;
+}
+.name {
+  color: #5d5755;
+  font-size: 1rem;
+  font-weight: 700;
+}
+.role {
+  background-color: #c21e39;
+  border-radius: 5rem;
+  color: #ffffff;
+  display: inline-block;
+  font-size: 0.7rem;
+  font-weight: 600;
+  padding: 0.2rem 1rem;
+}
+.message-btn {
+  border: 1px solid #cdcdcd;
+  height: 35px;
+  width: 35px;
+}
+.divider {
+  background-color: #f2e9e6;
+  border-radius: 2rem;
+  height: 0.5px;
+  width: 100%;
+}
 </style>
