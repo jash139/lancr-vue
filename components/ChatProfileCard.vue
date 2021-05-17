d1e463e214239af27d921f50fac8bd8a803a9d59
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-18 01:25:59 +0530

    Add content to card and import in list

--- components/ChatProfileCard.vue
+++ components/ChatProfileCard.vue
@@ -1,5 +1,22 @@
 <template>
-  <h2>chatprofile card</h2>
+  <NuxtLink to="/chats/123userid">
+    <div>
+      <div class="chat-profile-card">
+        <div class="body">
+          <img
+            src="https://images.unsplash.com/photo-1563497425252-36b755215241?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+            alt="Avatar"
+            class="avatar"
+          />
+          <div class="details">
+            <h2 class="name">Doggo Gang</h2>
+            <div class="role">Underworld don</div>
+          </div>
+        </div>
+      </div>
+      <div class="divider" />
+    </div>
+  </NuxtLink>
 </template>
 
 <script>
@@ -9,4 +26,41 @@ export default {
 </script>
 
 <style scoped>
+.chat-profile-card {
+  margin: 1rem 0;
+}
+.body {
+  display: flex;
+}
+.details {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+  margin-left: 1rem;
+}
+.avatar {
+  border-radius: 20rem;
+  max-width: 50px;
+}
+.name {
+  color: #5d5755;
+  font-size: 1rem;
+  font-weight: 700;
+}
+.role {
+  background-color: #c21e39;
+  border-radius: 5rem;
+  color: #ffffff;
+  display: inline-block;
+  font-size: 0.7rem;
+  font-weight: 600;
+  padding: 0.2rem 1rem;
+}
+.divider {
+  background-color: #f2e9e6;
+  border-radius: 2rem;
+  height: 0.5px;
+  width: 100%;
+}
 </style>
--- components/ChatProfileList.vue
+++ components/ChatProfileList.vue
@@ -2,18 +2,34 @@
   <div class="chat-profile-list">
     <h2 class="chat-heading">Chat</h2>
     <!-- Additional feature idea - add search names field -->
+    <ChatProfileCard />
+    <ChatProfileCard />
+    <ChatProfileCard />
+    <ChatProfileCard />
+    <ChatProfileCard />
+    <ChatProfileCard />
+    <ChatProfileCard />
+    <ChatProfileCard />
+    <ChatProfileCard />
+    <ChatProfileCard />
+    <ChatProfileCard />
   </div>
 </template>
 
 <script>
+import ChatProfileCard from "./ChatProfileCard";
+
 export default {
   name: "ChatProfileList",
+  components: {
+    ChatProfileCard,
+  },
 };
 </script>
 
 <style scoped>
 .chat-heading {
-  padding-bottom: 2rem;
+  padding-bottom: 1rem;
 }
 .chat-profile-list {
   border-right: 2px solid #f2e9e6;
