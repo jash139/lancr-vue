7ebdcc88449aaf0cc0356d17178c6a4e0aa3b197
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-18 18:04:41 +0530

    Create chat button

---
+++ components/ChatButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-button type="is-light" size="is-medium" class="chat-button">
+    <b-icon icon="chat"> </b-icon>
+  </b-button>
+</template>
+
+<script>
+export default {
+  name: "ChatButton",
+};
+</script>
+
+<style scoped>
+.chat-button {
+  background-color: #c21e39;
+  border-radius: 5rem;
+  color: #ffffff;
+  position: fixed;
+  bottom: 1.5rem;
+  right: 1.5rem;
+  height: 50px;
+  width: 50px;
+  z-index: 5;
+}
+.chat-button:hover {
+  background-color: #c21e39;
+  color: #ffffff;
+}
+.chat-button:focus {
+  color: #ffffff;
+}
+</style>
