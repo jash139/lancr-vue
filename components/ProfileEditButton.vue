dca19eab6542e79c00e4a0276ae18b21e7f953e0
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-19 03:53:50 +0530

    Add modal open on button click

--- components/ProfileEditButton.vue
+++ components/ProfileEditButton.vue
@@ -1,10 +1,25 @@
 <template>
-  <button class="btn primary-btn btn-shadow edit-btn">Edit Details</button>
+  <div>
+    <button
+      class="btn primary-btn btn-shadow edit-btn"
+      @click="isModalActive = true"
+    >
+      Edit Details
+    </button>
+    <b-modal v-model="isModalActive" :width="640" scroll="keep">
+      <div class="edit-modal">card content</div>
+    </b-modal>
+  </div>
 </template>
 
 <script>
 export default {
   name: "ProfileEditButton",
+  data() {
+    return {
+      isModalActive: false,
+    };
+  },
 };
 </script>
 
