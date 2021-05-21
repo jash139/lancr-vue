7bf1e6ac96b5171555807fd7c2f77dd1e746150d
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-22 03:56:15 +0530

    Add user and project structure to initial state

--- store/state.js
+++ store/state.js
@@ -1,7 +1,61 @@
+// Default user structure as specified in database schema
+
+const userStructure = {
+    uid: "",
+    name: "",
+    title: "",
+    profilePicture: "",
+    about: "",
+    dateOfBirth: "",
+    rating: "",
+    experience: [
+        {
+            organization: "",
+            role: "",
+            start: "",
+            end: ""
+        }
+    ],
+    contact: {
+        phone: "",
+        location: { country: "", state: "", city: "" },
+        email: ""
+    },
+    connections: [],
+    projects: [],
+    skills: [],
+    languages: []
+};
+
+// Default project structure as specified in database schema
+
+const projectStructure = {
+    uid: "",
+    title: "",
+    description: "",
+    status: "",
+    requirements: [],
+    offeredAmount: {
+        currencyType: "",
+        start: "",
+        end: ""
+    },
+    timePeriod: {
+        start: "",
+        end: ""
+    },
+    location: {
+        country: "",
+        state: "",
+        city: ""
+    },
+    applicants: []
+};
+
 export default function () {
     return {
-        currentUser: null,
-        project: {},
+        currentUser: userStructure,
+        project: projectStructure,
         allProjects: [],
         allFreelancers: []
     };
