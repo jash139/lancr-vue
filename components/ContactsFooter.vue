9d9f24e064c543be7479f58f0949059264907a90
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-24 01:17:18 +0530

    Add functions to handle button click

--- components/ContactsFooter.vue
+++ components/ContactsFooter.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="contacts-footer">
     <div class="stroke" />
-    <v-btn icon large class="contact-btn">
+    <v-btn icon large class="contact-btn" @click="handleClick(linkedin)">
       <LinkedinIcon />
     </v-btn>
-    <v-btn icon large class="contact-btn">
+    <v-btn icon large class="contact-btn" @click="handleClick(github)">
       <GithubIcon />
     </v-btn>
-    <v-btn icon large class="contact-btn">
+    <v-btn icon large class="contact-btn" @click="handleEmailClick(email)">
       <EmailIcon />
     </v-btn>
   </div>
@@ -17,13 +17,38 @@
 import LinkedinIcon from "./LinkedinIcon";
 import GithubIcon from "./GithubIcon";
 import EmailIcon from "./EmailIcon";
+import homeContent from "../assets/homeContent";
+import { openTab, copyEmail } from "../assets/methods";
+import { mapActions } from "vuex";
 
 export default {
   name: "ContactsFooter",
+  data() {
+    return {
+      linkedin: "",
+      github: "",
+      email: "",
+    };
+  },
   components: {
     LinkedinIcon,
     GithubIcon,
     EmailIcon,
+  },
+  methods: {
+    ...mapActions(["showNotificationMessage"]),
+    handleClick(link) {
+      openTab(link);
+    },
+    handleEmailClick(link) {
+      copyEmail(link);
+      this.showNotificationMessage("Email copied!");
+    },
+  },
+  created() {
+    this.linkedin = homeContent.contactLinks.linkedin;
+    this.github = homeContent.contactLinks.github;
+    this.email = homeContent.contactLinks.email;
   },
 };
 </script>
