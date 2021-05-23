a006d62bb3847b7934862d00bc3e067e4589d90e
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-24 00:05:55 +0530

    Create snackbar for notification

---
+++ components/Snackbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  methods: {
+    notify(message) {
+      this.$buefy.snackbar.open({
+        duration: 3000,
+        message: message,
+        type: "is-danger",
+        position: "is-bottom-left",
+        actionText: "Ok",
+      });
+    },
+  },
+  created() {
+    this.unsubscribe = this.$store.subscribe((mutation, state) => {
+      if (mutation.type === "setNotification") {
+        this.notify(state.simple);
+      }
+    });
+  },
+  beforeDestroy() {
+    this.unsubscribe();
+  },
+};
+</script>
+
+<style scoped>
+</style>
