125f7f9ebacf198dbf6acb9414f56c45c4c1cc83
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-23 23:14:08 +0530

    Add copy email function

--- assets/methods.js
+++ assets/methods.js
@@ -12,9 +12,18 @@ const openTab = (url) => {
     window.open(url, '_blank')
 };
 
+const copyEmail = (email) => {
+    var tempInput = document.createElement("input");
+    tempInput.value = email;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+};
 
 export {
     scrollToTop,
     scrollToSection,
     openTab,
+    copyEmail,
 };
