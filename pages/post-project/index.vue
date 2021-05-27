ac94143b32ded32ffc30776ea3c4f824d5fe1e2c
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-28 03:41:56 +0530

    Add basic layout

--- pages/post-project/index.vue
+++ pages/post-project/index.vue
@@ -1,10 +1,31 @@
 <template>
-  <div>post project</div>
+  <div class="root">
+    <AppBar />
+    <div class="content">
+      <BackButton />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import AppBar from "../../components/AppBar";
+import BackButton from "../../components/BackButton";
+
+export default {
+  components: {
+    AppBar,
+    BackButton,
+  },
+};
 </script>
 
 <style scoped>
+.root {
+  min-height: 100vh;
+  padding-bottom: 1.5rem;
+}
+.content {
+  max-width: 95%;
+  margin: auto;
+}
 </style>
