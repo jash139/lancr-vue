11c9c52d260648648b69ca30d791223395c32d9b
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-28 19:19:34 +0530

    Add post-project to middleware protected route

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,7 +8,7 @@ export default function ({ app, route, redirect }) {
             }
         } else {
             // No user 
-            if (route.path.includes('/chats') || route.path.includes('/freelancers') || route.path.includes('/profile') || route.path.includes('/projects')) {
+            if (route.path.includes('/chats') || route.path.includes('/freelancers') || route.path.includes('/profile') || route.path.includes('/projects') || route.path.includes('/post-project')) {
                 redirect('/signin');
             }
         }
