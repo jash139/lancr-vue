ccffdb08c3ea36051f9c1ce6452ea49357f54c7e
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-31 18:49:38 +0530

    Create freelancer tabs

---
+++ components/FreelancerTabs.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="freelancer-tabs">
+    <div class="tab">
+      <button
+        class="tablinks active"
+        @click="changeTab($event, 'freelancer-tab-1')"
+      >
+        Profile
+      </button>
+      <button class="tablinks" @click="changeTab($event, 'freelancer-tab-2')">
+        Projects
+      </button>
+      <button class="tablinks" @click="changeTab($event, 'freelancer-tab-3')">
+        Connections
+      </button>
+    </div>
+
+    <div id="freelancer-tab-1" class="tabcontent activetab">
+      <ProfileTabContent />
+    </div>
+
+    <div id="freelancer-tab-2" class="tabcontent">
+      <ProjectsTabContent />
+    </div>
+
+    <div id="freelancer-tab-3" class="tabcontent">
+      <ConnectionsTabContent />
+    </div>
+  </div>
+</template>
+
+<script>
+import ProfileTabContent from "./ProfileTabContent";
+import ProjectsTabContent from "./ProjectsTabContent";
+import ConnectionsTabContent from "./ConnectionsTabContent";
+
+export default {
+  name: "FreelancerTabs",
+  components: {
+    ProfileTabContent,
+    ProjectsTabContent,
+    ConnectionsTabContent,
+  },
+  methods: {
+    changeTab: (evt, cityName) => {
+      var i, tabcontent, tablinks;
+      tabcontent = document.getElementsByClassName("tabcontent");
+      for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+      }
+      tablinks = document.getElementsByClassName("tablinks");
+      for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+      }
+      document.getElementById(cityName).style.display = "block";
+      evt.currentTarget.className += " active";
+    },
+  },
+};
+</script>
+
+<style scoped>
+.freelancer-tabs {
+  margin-top: 3rem;
+}
+.tab {
+  border-bottom: 2px solid #ffffff;
+  overflow: hidden;
+}
+.tab button {
+  background-color: transparent;
+  border: none;
+  border-bottom: 3px solid transparent;
+  color: #939498;
+  cursor: pointer;
+  float: left;
+  font-size: 1rem;
+  font-weight: 700;
+  outline: none;
+  padding: 0.6rem 2rem;
+  transition: all 0.2s ease;
+}
+.tab button:hover {
+  color: #5d5755;
+}
+.tab button.active {
+  border-bottom: 3px solid #c21e39;
+  color: #c21e39;
+}
+.tabcontent {
+  background-color: #ffffff;
+  box-shadow: 0 10px 6px rgba(0, 0, 0, 0.05);
+  border-radius: 3px;
+  display: none;
+  margin: 1.5rem 0;
+}
+.activetab {
+  display: block;
+}
+@media only screen and (max-width: 600px) {
+  .tab button {
+    padding: 0.6rem 1rem;
+  }
+}
+</style>
